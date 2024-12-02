7fe8784b1997f1255c1a9c357f5fdb43cad05dd6
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-12-01 22:55:10 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import FollowCursor from "@/components/followCursor";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <FollowCursor></FollowCursor>
+      <FollowCursor />
       <div>
         <h1 className="text-7xl font-bold text-center mb-2">Giacomo Marino</h1>
         <p className="text-2xl text-center">Full Stack Developer</p>
-        <div className="flex flex-row mb-2 justify-center">
-          <div className=" w-1/3 float-right sm:w-full md:w-2/4 lg:w-2/4 xl:w-2/4">
-            <p className="m-3 mb-5 mt-2 p-2 rounded border border-black border-opacity-0 dark:border-opacity-0 dark:border-white hover:bg-opacity-5 hover:border-opacity-20 hover:-opacity-90 hover:translate-x-1 hover:translate-y-1 hover:shadow-lg transition-all">
+        <div className="flex flex-col sm:flex-row sm:flex-wrap mb-2 justify-center">
+          <div className="w-full sm:w-2/3 md:w-2/4 lg:w-2/4 p-3">
+            <p className="mb-5 mt-2 p-2 rounded border border-black border-opacity-0 dark:border-opacity-0 dark:border-white hover:bg-opacity-5 hover:border-opacity-20 hover:-opacity-90 hover:translate-x-1 hover:translate-y-1 hover:shadow-lg transition-all">
               I am a full stack developer working in bioinformatics with a
               passion for data science and machine learning. I am currently
               working as a{" "}
@@ -20,7 +20,7 @@ export default function Home() {
                 Bioinformatics Software Engineer
               </span>{" "}
               at the Ma&apos;ayan Laboratory at the Icahn School of Medicine at
-              Mount Sinai, where I am involved in development of web severs and
+              Mount Sinai, where I am involved in development of web servers and
               bioinformatics tools. I graduated from Brown University in 2022
               with a <i>ScB </i> in Neuroscience & <i>AB </i> in Computer
               Science. I am particularly interested in the intersection of
@@ -29,9 +29,9 @@ export default function Home() {
               recent highlighted projects:
             </p>
           </div>
-          <div className="col-span-2 align-middle my-auto">
+          <div className="w-full sm:w-auto justify-center">
             <img
-              className="rounded-lg border-4 border-white-500  my-auto"
+              className="rounded-lg border-4 my-auto mx-auto border-white-500"
               src="headshot.jpg"
               width={175}
               height={175}
