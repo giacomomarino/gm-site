2b0fc992a9d4adfac60358ffd08ed88a242d39d4
Giacomo Marino <77803629+giacomomarino@users.noreply.github.com>
2025-07-11 09:57:38 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
         <div className="flex flex-col sm:flex-row sm:flex-wrap mb-2 justify-center">
           <div className="w-full sm:w-2/3 md:w-2/4 lg:w-2/4 p-3">
             <p className="mb-5 mt-2 p-2 rounded border border-black border-opacity-0 dark:border-opacity-0 dark:border-white hover:bg-opacity-5 hover:border-opacity-20 hover:-opacity-90 hover:translate-x-1 hover:translate-y-1 hover:shadow-lg transition-all transition-duration:2s">
-            I&apos;m a full-stack developer specializing in bioinformatics. For three years, I worked as a{" "}
+            I&apos;m a full-stack developer specializing in bioinformatics.  I graduated from Brown University in 2022 with an <i>ScB</i> in Neuroscience and an <i>AB</i> in Computer Science. I spent three years as a{" "}
             <span style={{ fontStyle: "italic" }}>
               Bioinformatics Software Engineer
             </span>{" "}
-            at the Ma&apos;ayan Laboratory at the Icahn School of Medicine at Mount Sinai, where I developed web servers and bioinformatics tools. 
+            at the Ma&apos;ayan Laboratory at the Icahn School of Medicine at Mount Sinai, where I built web servers and data-driven tools for biomedical research.
 
-            I graduated from Brown University in 2022 with an <i>ScB</i> in Neuroscience and an <i>AB</i> in Computer Science. I&apos;m now headed to Yale University to pursue a PhD in the Computational Biology and Biomedical Informatics program.
+            I&apos;m now headed to Yale University to pursue a PhD in the Computational Biology and Biomedical Informatics program.
 
             Check out one of my recent projects:
             </p>
